42df9c5455ac9e438af407037a5f8ee32e6e37b2
re1nth <re1nth98@gmail.com>
2019-02-03 03:02:15 +0530

    Logs for EDF and RMS

--- edf.cpp
+++ edf.cpp
@@ -5,66 +5,150 @@ using namespace std;
 struct Event
 {
 	int process_id;
-	float interrupt;
+	int interrupt;
+	int process_time;
 	int action;
+	int period;
 };
 
 struct Process
 {
 	int process_id;
-	float process_time;
+	int process_time;
+	int deadline;
 	int period;
 };
 
 struct Fuple
 {
 	int process_id;
-	float process_time;
+	int process_time;
 	int period;
 	int num_of_times;
 };
 
-vector<Event> Event_queue;
+vector<Event> Event_list;
 
 queue<Process> Process_queue;
 
 vector<Fuple>Store;
 
+Process running;
+
 bool compare(Event e1, Event e2)
 {
-	return(e1.interrupt<e2.interrupt);
+	return (e1.interrupt<e2.interrupt);
+}
+
+void next_process()
+{
+	int cnt=0,sz=Process_queue.size();
+
+	int deadline_temp=INT_MAX;
+
+	while(cnt<sz)
+	{
+		Process ref=Process_queue.front();	
+
+		if(deadline_temp==INT_MAX||(ref.deadline<deadline_temp))
+		{
+			running=ref;
+
+			deadline_temp=ref.deadline;
+		}
+
+		Process_queue.pop();
+
+		Process_queue.push(ref);
+
+		cnt++;
+	}
+
+	if(deadline_temp==INT_MAX)
+	{
+		running.process_id=-1;
+
+		running.process_time=INT_MAX;
+
+		running.deadline=-1;
+
+		running.period=INT_MAX;
+	}
+}
+
+void update(int process_id,int time_executes)
+{	
+	int cnt=0,sz=Process_queue.size();
+
+	while(cnt<sz)
+	{
+		Process ref=Process_queue.front();	
+
+		Process_queue.pop();
+
+		if(ref.process_id==process_id)
+		{
+			if(time_executes!=-1)
+			{
+				if(ref.process_time-time_executes>0)
+				{
+					ref.process_time=(ref.process_time-time_executes);
+
+					Process_queue.push(ref);	
+				}
+			}
+		}	
+
+		else Process_queue.push(ref);
+
+		cnt++;
+	}
 }
 
 int main()
 {
 	int num_of_process,cnt=0; 
 
-	FILE *fptr=NULL;
+	FILE *fptr1=NULL;
 
-	fptr=fopen("inp-params.txt","r");
+	FILE *fptr2=NULL;
 
-	fscanf(fptr,"%d",&num_of_process);
+	fptr1=fopen("inp-params.txt","r");
+
+	fptr2=fopen("ED-Log.txt","w");
+
+	fscanf(fptr1,"%d",&num_of_process);
 
 	int process_id,period,num_of_times;
 
-	float process_time;
+	int process_time;
+
+	int prev_process=-1;
+
+	int prev_left=0;
+
+	int tim[num_of_process+1];
 
 	while(cnt<num_of_process)
 	{
-		fscanf(fptr,"%d %f %d %d",&process_id,&process_time,&period,&num_of_times);
+		fscanf(fptr1,"%d %d %d %d",&process_id,&process_time,&period,&num_of_times);
 
-		Fuple *temp=new Fuple;
+		fprintf(fptr2,"Process P%d: processing time=%d; deadline:%d, period=%d joined the system at time 0\n",process_id,(int)process_time,period,period);
 
-		*temp={process_id,process_time,period,num_of_times};
+		Fuple temp;
 
-		Store.push_back(*temp);
+		temp={process_id,process_time,period,num_of_times};
+
+		tim[process_id]=process_time;
+
+		Store.push_back(temp);
 
 		cnt++;
 	}
 	
-	fclose(fptr);	
+	Event event_temp;
 
-	Event *event_temp=new Event;
+	Process process_temp;
 
 	cnt=0;
 
@@ -80,29 +164,145 @@ int main()
 
 		for(int i=0;i<num_of_times;i++)
 		{
-			event_temp->process_id=process_id;
+			event_temp.process_id=process_id;
 
-			event_temp->interrupt=(i*period);
+			event_temp.interrupt=(i*period);
 
-			event_temp->action=(1);
+			event_temp.process_time=(process_time);
 
-			Event_queue.push_back(*event_temp);
+			event_temp.action=(1);
+
+			event_temp.period=period;
+
+			Event_list.push_back(event_temp);
 		
-			event_temp->process_id=process_id;
+			event_temp.process_id=process_id;
 
-			event_temp->interrupt=((i+1)*period);
+			event_temp.interrupt=((i+1)*period);
 
-			event_temp->action=(-1);
+			event_temp.process_time=process_time;
 
-			Event_queue.push_back(*event_temp);
+			event_temp.period=period;
+
+			event_temp.action=(-1);
+
+			Event_list.push_back(event_temp);
 		}
 
 		cnt++;
 	}
 
-	sort(Event_queue.begin(),Event_queue.end(),compare);
+	sort(Event_list.begin(),Event_list.end(),compare);
 
-	delete event_temp;
+	for(auto it=Event_list.begin();it!=Event_list.end();)
+	{
+		int interrupt=it->interrupt;
+
+		if(!Process_queue.empty())next_process();
+
+		queue<Process>Temp_queue;
+
+		while(it->interrupt==interrupt && it!=Event_list.end())
+		{	
+			if(it->action==1)
+			{
+				process_temp.process_id=it->process_id;
+
+				process_temp.process_time=it->process_time;
+
+				process_temp.period=it->period;
+
+				process_temp.deadline=it->interrupt+it->period;
+
+				Temp_queue.push(process_temp);
+			}
+
+			else
+			{
+				update(it->process_id,-1);
+			}
+
+			it++;
+		}	
+
+		while(!Temp_queue.empty())
+		{
+			Process_queue.push(Temp_queue.front());
+
+			Temp_queue.pop();
+		}
+
+		if(it==Event_list.end()) break;
+
+		int curr=interrupt;
+
+		int deadline=it->interrupt;
+
+		cnt=0; 
+
+		int sz=Process_queue.size();
+
+		if(!Process_queue.empty()) next_process();
+
+		if(running.process_id==-1) break;
+
+		int execution_time=running.process_time;
+
+		do		
+		{
+			if(prev_process!=-1)
+			{
+				if(prev_process!=running.process_id)
+				{
+					fprintf(fptr2,"Process P%d is preempted by P%d at %d. Remaining process time:%d \n",prev_process,running.process_id,(int)curr,(int)prev_left);
+				}
+			}
+
+			if(running.process_time==tim[running.process_id])
+			{
+				fprintf(fptr2,"Process P%d starts execution at %d \n",running.process_id,(int)curr);	
+			}
+
+			else
+			{
+				if(prev_process!=running.process_id)
+				{
+					fprintf(fptr2,"Process P%d resumes execution at %d \n",running.process_id,(int)curr);
+				}
+			}			
+	
+			if(curr+execution_time<=deadline)
+			{
+				update(running.process_id,-1);			
+
+				fprintf(fptr2,"Process P%d finishes execution at %d \n",running.process_id,(int)(curr+execution_time));
+			
+				prev_process=-1;
+			}
+
+			else 
+			{
+				prev_process=running.process_id;
+
+				prev_left=running.process_time-(deadline-curr);
+
+				update(running.process_id,deadline-curr);
+			}
+
+			next_process();
+
+			curr=curr+execution_time;
+
+			if(running.process_id!=-1) execution_time=running.process_time;
+		
+			else break;
+
+		} while(curr<deadline);	
+	}
+
+	fclose(fptr1);
+
+	fclose(fptr2);
 
 	return 0;
 }
